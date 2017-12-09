8d002480df78a003fdaa410b1672069218e3dc30
LouWii <link14.online@gmail.com>
2017-12-08 16:36:56 -0800

    install and setup css minification in gulp

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -11,6 +13,15 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
+gulp.task('minify-css', () => {
+  return gulp.src('css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'));
+});
+
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./scss/**/*.scss', ['sass', 'minify-css']);
 });
