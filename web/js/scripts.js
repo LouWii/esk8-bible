1253eca9ce650c5d7f41388f1cd9ac9d42149f46
Louwii <link14.online@gmail.com>
2019-06-14 23:17:17 -0700

    improve chart display, especially the tooltip

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -101,7 +101,8 @@ $(function() {
       window.currentChartPart = 0;
 
       // Setup chart container styling (manually set the width for best look)
-      var calculatedWidth = window.labels[window.currentChartPart].length * 2;
+      const pixelsPerPoint = 3;
+      var calculatedWidth = window.labels[window.currentChartPart].length * pixelsPerPoint;
       chartCanvas.css('width', calculatedWidth+'px');
       
       chartCanvas.css('height', 300+'px');
@@ -135,6 +136,76 @@ $(function() {
         }
       });
 
+      const customTooltips = function(tooltip) {
+        // Tooltip Element
+        var tooltipEl = document.getElementById('chartjs-tooltip');
+
+        if (!tooltipEl) {
+          tooltipEl = document.createElement('div');
+          tooltipEl.id = 'chartjs-tooltip';
+          tooltipEl.innerHTML = '<table></table>';
+          this._chart.canvas.parentNode.appendChild(tooltipEl);
+        }
+
+        var container = tooltipEl.parentElement;
+
+        // Hide if no tooltip
+        if (tooltip.opacity === 0) {
+          tooltipEl.style.opacity = 0;
+          return;
+        }
+
+        // Set caret Position
+        tooltipEl.classList.remove('above', 'below', 'no-transform');
+        if (tooltip.yAlign) {
+          tooltipEl.classList.add(tooltip.yAlign);
+        } else {
+          tooltipEl.classList.add('no-transform');
+        }
+  
+        function getBody(bodyItem) {
+          return bodyItem.lines;
+        }
+
+        // Set Text
+        if (tooltip.body) {
+          var titleLines = tooltip.title || [];
+          var bodyLines = tooltip.body.map(getBody);
+
+          var innerHtml = '<thead>';
+
+          titleLines.forEach(function(title) {
+            innerHtml += '<tr><th>' + title + '</th></tr>';
+          });
+          innerHtml += '</thead><tbody>';
+
+          bodyLines.forEach(function(body, i) {
+            var colors = tooltip.labelColors[i];
+            var style = 'background:' + colors.backgroundColor;
+            style += '; border-color:' + colors.borderColor;
+            style += '; border-width: 2px';
+            var span = '<span class="line-color-key" style="' + style + '"></span>';
+            innerHtml += '<tr><td>' + span + body + '</td></tr>';
+          });
+          innerHtml += '</tbody>';
+  
+          var tableRoot = tooltipEl.querySelector('table');
+          tableRoot.innerHTML = innerHtml;
+        }
+
+        var positionY = this._chart.canvas.offsetTop;
+        var positionX = this._chart.canvas.offsetLeft;
+  
+        // Display, position, and set styles for font
+        tooltipEl.style.opacity = 1;
+        tooltipEl.style.left = positionX + tooltip.caretX - container.scrollLeft + 5 + 'px';
+        // tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+        tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+        tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
+        tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+        tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+      };
+
       const vescChart = new Chart(ctx, {
         type: 'LineWithLine',
         data: {
@@ -144,14 +215,11 @@ $(function() {
         options: {
           responsive: false,
           tooltips: {
+            enabled: false,
             mode: 'nearest',
             intersect: false,
             axis: 'x',
-            custom: function(tooltipModel) {
-              if (tooltipModel.title) {
-
-              }
-            },
+            custom: customTooltips,
             callbacks: {
               label: function (tooltipItem, data) {
                 let labelStr = '';
